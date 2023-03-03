0a417826da585bc5ce2fd94441354a4533d35eff
Albert Mensah <zamensah840@gmail.com>
2023-03-03 09:19:47 +0000

    check contact availablity

--- controllers/contact_controller.js
+++ controllers/contact_controller.js
@@ -10,11 +10,15 @@ exports.addContact = catchAsync(async (req, res) => {
 
   let account = await Account.findById(req.params.id);
 
-  contact.accounts.push(req.params.id);
-  account.contacts.push(contact._id);
+  const checkAvailability = account.contacts.some((res) => contact._id === res);
 
-  await account.save();
-  await contact.save();
+  if (!checkAvailability) {
+    contact.accounts.push(req.params.id);
+    account.contacts.push(contact._id);
+
+    await account.save();
+    await contact.save();
+  }
 
   res
     .status(200)
@@ -39,11 +43,15 @@ exports.addAccountToContact = catchAsync(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
   const account = await Account.findById(req.body._id);
 
-  account.contacts.push(contact._id);
-  contact.accounts.push(account._id);
+  const checkAvailability = account.contacts.some((res) => contact._id === res);
 
-  await contact.save();
-  await account.save();
+  if (!checkAvailability) {
+    account.contacts.push(contact._id);
+    contact.accounts.push(account._id);
+
+    await contact.save();
+    await account.save();
+  }
 
   res.status(200).json({ status: "success", contact });
 });
